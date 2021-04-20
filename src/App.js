7608b3076ab72e16b5734ca029b16700a196624b
RodriguesRodrigo <rrscotti@hotmail.com>
2021-04-20 00:07:10 -0300

    using props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Button from "./Button";
 function App() {
   return (
     <>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
+      <Button title="title 1"></Button>
+      <Button title="title 2"></Button>
+      <Button title="title 3"></Button>
+      <Button title="title 4"></Button>
+      <Button title="title 5"></Button>
     </>
   );
 }
