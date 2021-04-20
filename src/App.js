b484ef46c90470600f340e36263c99c9d5387434
RodriguesRodrigo <rrscotti@hotmail.com>
2021-04-20 00:08:54 -0300

    using children on component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Button from "./Button";
 function App() {
   return (
     <>
-      <Button title="title 1"></Button>
-      <Button title="title 2"></Button>
-      <Button title="title 3"></Button>
-      <Button title="title 4"></Button>
-      <Button title="title 5"></Button>
+      <Button>title 1</Button>
+      <Button>title 2</Button>
+      <Button>title 3</Button>
+      <Button>title 4</Button>
+      <Button>title 5</Button>
     </>
   );
 }
