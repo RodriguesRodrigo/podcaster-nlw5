21a3843645d02b0bdd2ceb870c95c9a60303b090
RodriguesRodrigo <rrscotti@hotmail.com>
2021-04-25 23:23:48 -0300

    pulando o tempo do audio conforme alterado no slider

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -52,6 +52,11 @@ export function Player() {
     })
   }
 
+  function handleSeek(amount: number) {
+    audioRef.current.currentTime = amount;
+    setProgress(amount);
+  }
+
   return (
     <div className={styles.playerContainer}>
       <header>
@@ -84,6 +89,7 @@ export function Player() {
               <Slider
                 max={episode.duration}
                 value={progress}
+                onChange={handleSeek}
                 trackStyle={{ backgroundColor: '#04D361' }}
                 railStyle={{ backgroundColor: '#9F75FF' }}
                 handleStyle={{ borderColor: '#04D361', borderWidth: 4 }}
